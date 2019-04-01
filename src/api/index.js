0b425a9aa29116b22d0fc70273039242f750795c
Diego Vallejo <die.1702@hotmail.com>
2019-04-01 13:32:48 -0600

    client Api

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,50 @@
-const apiURL = "https://api-sda.asd.net";
+const apiURL = "https://mongas.norman404.dev";
 
 class Api {
     constructor(baseUrl) {
         this.apiURL = baseUrl;
     }
 
+    buildRequestUrl = (urlString, params) => {
+        return new URL(`${this.apiURL}${urlString}/${Object.keys(params).map(key => params[key]).join("/")}`);
+    };
 
-    exmple = async (entry) => {
-        const params = {entry};
+    // --------------------------------------- functions  ---------------------------------------
+
+    getTeamList = async () => {
+        const params = {};
         const config = {method: 'GET'};
-        const request = await fetch(this.buildRequestUrl('/funcName', params).toString(), config);
+        const request = await fetch(this.buildRequestUrl('/user', params).toString(), config);
+        if (request.status > 399) {
+            throw new Error(request.status.toString());
+        }
+        return request.json();
+    };
+
+    getCategories = async () => {
+        const params = {};
+        const config = {method: 'GET'};
+        const request = await fetch(this.buildRequestUrl('/categoria', params).toString(), config);
+        if (request.status > 399) {
+            throw new Error(request.status.toString());
+        }
+        return request.json();
+    };
+
+    getEventInfo = async (uuid = {}) => {
+        const params = uuid;
+        const config = {method: 'GET'};
+        const request = await fetch(this.buildRequestUrl('/evento', params).toString(), config);
+        if (request.status > 399) {
+            throw new Error(request.status.toString());
+        }
+        return request.json();
+    };
+
+    getGameInfo = async (game = {}) => {
+        const params = game;
+        const config = {method: 'GET'};
+        const request = await fetch(this.buildRequestUrl('/juego', params).toString(), config);
         if (request.status > 399) {
             throw new Error(request.status.toString());
         }
